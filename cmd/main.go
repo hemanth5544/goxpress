ee1d2a51df8165b74130ca0ca8e5aa59f35d8cde
Hemanth Rachapalli <rachapalli.hemanth5544@gmail.com>
2025-12-08 15:30:18 +0530

    setup gofiber

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/hemanth5544/goxpress/internal/db"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"msg": "Hello Hemanth this side",
+		})
+	})
+	db.ConnectDB("Hemanth")
+
+	fmt.Println("new server is created")
+	app.Listen(":3000")
+}
---
+++ internal/db/db.go
@@ -0,0 +1,13 @@
+package db
+
+import (
+	"fmt"
+
+
+)
+
+
+func ConnectDB( x string) {
+	fmt.Println("Db connection will be started",x)
+
+}
