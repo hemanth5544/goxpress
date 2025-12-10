20b273f01ee0aa0ec30bbe1f3ca010a8b750d5f8
Hemanth Rachapalli <rachapalli.hemanth5544@gmail.com>
2025-12-10 23:33:59 +0530

    repo auth

--- internal/auth/repository/auth_repository.go
+++ internal/auth/repository/auth_repository.go
@@ -1,1 +1,72 @@
 package repository
+
+import (
+	"github.com/hemanth5544/goxpress/internal/auth/model"
+	"gorm.io/gorm"
+)
+
+// IAuthRepository is an interface that defines the methods for user authentication and management.
+// in service we will use this interface to call the methods
+type IAuthRepository interface {
+	CreateUser(userRequest model.User) error
+	CheckUserExist(email string) (*model.User, error)
+	GetUserById(id int) (*model.User, error)
+}
+
+type AuthRepository struct {
+	db *gorm.DB
+}
+
+//remember theat in GO we use all varaibiles rather using the module directly in this gorm.DB is passed to db
+//this helps that Go had clean arch wiht no golabe deoency or gloabal vabrle just use when need dependcy injection when neede not always
+
+// in GO we dont have classes so taht constictor s also is a fun the
+// here NewAuthRepository is acting like constrict and  injecting db as a property to our AuthRepository
+// so that we can use db every where use AuthRepository
+func NewAuthRepository(db *gorm.DB) *AuthRepository {
+	return &AuthRepository{db: db}
+}
+
+//---here r *AuthRepository is recvier  imaginere recvier
+//like  a mehtod attched to this funtion createuesr so that you can acces AuthRepository properties in ypur
+//createuesr  fun
+//--------
+
+func (r *AuthRepository) CreateUser(userRequest model.User) error {
+
+	if err := r.db.Create(&userRequest).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *AuthRepository) CheckUserExist(email string) (*model.User, error) {
+
+	var user model.User
+
+	if err := r.db.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+
+}
+
+func (r *AuthRepository) GetUserById(id int) (*model.User, error) {
+
+	var user model.User
+
+	result := r.db.Find(&user, id)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	userResponse := model.User{
+		Username: user.Username,
+		Email:    user.Email,
+	}
+
+	return &userResponse, nil
+
+}
