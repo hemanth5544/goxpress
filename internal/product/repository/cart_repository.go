069aafc002f05a30dcdcd44aee9354fd5b9bfb47
Hemanth Rachapalli <rachapalli.hemanth5544@gmail.com>
2025-12-12 17:34:27 +0530

    new repos

--- internal/product/repository/cart_repository.go
+++ internal/product/repository/cart_repository.go
@@ -70,3 +70,54 @@ func (r *CartRepository) UpdateCartItem(item model.CartItem) error {
 
 	return r.db.Save(&item).Error
 }
+
+func (r *CartRepository) GetCartItem(userID uint) (*model.Cart, error) {
+	var cart model.Cart
+
+	result := r.db.Where("user_id = ?", userID).Preload("Items").First(&cart)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &cart, nil
+}
+
+func (r *CartRepository) FindCartItem(cartID uint, productID uint) (*model.CartItem, error) {
+	var item model.CartItem
+	result := r.db.Where("cart_id = ? AND product_id = ?", cartID, productID).First(&item)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &item, nil
+}
+
+func (r *CartRepository) RemoveCartItem(cartID uint, itemID uint) error {
+
+	return r.db.Where("cart_id = ? AND product_id = ?", cartID, itemID).Delete(&model.CartItem{}).Error
+
+}
+
+func (r *CartRepository) UpdateItemQuantity(productID uint, cartID uint, quantity int) error {
+
+	return r.db.Model(&model.CartItem{}).
+		Where("cart_id = ? AND product_id = ?", cartID, productID).
+		Update("quantity", quantity).Error
+}
+
+func (r *CartRepository) GetCartByUserID(userID uint) (model.Cart, error) {
+	var cart model.Cart
+	if err := r.db.Preload("Items").Where("user_id = ?", userID).First(&cart).Error; err != nil {
+		return model.Cart{}, err
+	}
+
+	return cart, nil
+}
+
+func (r *CartRepository) ClearCart(cartID uint) error {
+	if err := r.db.Where("cart_id = ?", cartID).Delete(&model.CartItem{}).Error; err != nil {
+		return err
+	}
+	return nil
+}
