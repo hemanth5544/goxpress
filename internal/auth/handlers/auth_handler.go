d36c545681837e38f629d30c032a1991562cf99e
Hemanth Rachapalli <rachapalli.hemanth5544@gmail.com>
2025-12-12 14:21:34 +0530

    login handler

--- internal/auth/handlers/auth_handler.go
+++ internal/auth/handlers/auth_handler.go
@@ -32,3 +32,54 @@ func (h *AuthHandler) RegisterUser(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusCreated, dto.AuthResponse{Message: "Register Success"})
 }
+
+func (h *AuthHandler) RegisterAdmin(ctx *gin.Context) {
+
+	var request dto.RegisterRequest
+
+	//should bind this like using req.body like use node
+	//here we dont use ShouldBindJSON it will pass the empty dto.RegisterRequest and db operarion will fail
+
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, dto.AuthResponse{Message: "Inavlid body Request"})
+	}
+	//Gin context has AbortWithStatusJSON it will stop the whole function
+	//like simple try catch where we cathed error and retrn
+	//for more
+	//https://pkg.go.dev/github.com/gin-gonic/gin#Context
+	if err := h.service.Register(request.Username, request.Email, request.Password, "admin"); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, dto.AuthResponse{Message: err.Error()})
+		return
+	}
+	//this net http module will have an abstracitn of http code here
+	//it will send 201 code there are mapping https://go.dev/src/net/http/status.go pls have a look
+	ctx.JSON(http.StatusCreated, dto.AuthResponse{Message: "Register Success"})
+
+}
+
+func (h *AuthHandler) Login(ctx *gin.Context)  {
+	/**
+	*see this is a serailition & deserailaton
+	*we often see the clinet will interact with the JSON to api acuse browser env will have js% json
+	*but out Golang will only impliclty untestnad structs this validation is verty imprtant
+	 */
+	var request dto.LoginRequest
+	if err := ctx.ShouldBindBodyWithJSON(&request); err != nil {
+
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, dto.AuthResponse{Message: "Inavlid body Request"})
+
+	}
+
+	token, err := h.service.Login(request)
+
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, dto.AuthResponse{Message: err.Error()})
+
+	}
+
+	//? res.setCookie()
+	ctx.SetCookie("Authorization", token, 3600, "/", "localhost", false, true)
+	
+	//? res.status(200).json(result)
+	ctx.JSON(http.StatusOK,dto.LoginResponse{Token: token})
+}
